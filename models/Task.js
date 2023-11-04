a5ed9594f0112bc954a1fad73a95929607caebe2
Salma Ahmed <salmahamzaahmed77@gmail.com>
2023-11-04 14:15:00 +0200

    CURD

--- models/Task.js
+++ models/Task.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const TaskSchema = new mongoose.Schema({
     name: {
+    //build-in validator
         type: String,
         required: [true, 'must provide name'],
         trim: true,
